41ba3810bcdc45a745969ef1da82c27f8a57f9fa
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-12-09 14:37:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1071,7 +1071,12 @@ if st.sidebar.checkbox("FinViz Data Viewer"):
         async with RetryClient(raise_for_status=False, retry_options=retry_options) as session:
             tasks = [fetch_quote_data(ticker, data_types, session) for ticker in tickers]
             return await asyncio.gather(*tasks, return_exceptions=True)
-    def filter_dataframe(df, unique_key_prefix):
+
+        def filter_dataframe(df, unique_key_prefix):
+        """
+        Creates a UI for filtering a DataFrame.
+        Preserves filter state using Streamlit session_state.
+        """
         modify = st.checkbox("Add Filters", key=f"{unique_key_prefix}_add_filters")
         if not modify:
             return df
