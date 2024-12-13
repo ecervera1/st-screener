59ec6d8ef4cdcd15df4c6ab1c9257f14bcd0b11f
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-12-13 13:50:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -173,16 +173,10 @@ custom_css = """
 st.markdown(custom_css, unsafe_allow_html=True)
 
 # Add logo
-st.markdown('<div class="center">', unsafe_allow_html=True)
-logo=st.image("Cervera Logo BWG.png", width=200, caption="")
-
-
+# st.image("Cervera Logo BWG.png", width=200, caption="")
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
-    st.image(logo)
-
-st.markdown('</div>', unsafe_allow_html=True)
-
+    st.image(st.image("Cervera Logo BWG.png", width=200, caption=""))
 st.markdown('<div class="title">Portfolio Management</div>', unsafe_allow_html=True)
 st.markdown('<div class="subheader">Stock Comparative Analysis</div>', unsafe_allow_html=True)
 st.markdown('<div class="caption">by Eli Cervera</div>', unsafe_allow_html=True)
