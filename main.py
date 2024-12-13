4cae4a418c6f88a6ca5b5670ce3e42e88e3b12b3
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-12-13 14:02:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -177,9 +177,12 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # st.image("Cervera Logo BWG.png", width=150, caption="")
 
 logo = "Cervera Logo BWG.png"  # Replace with the correct path to your image
-col1, col2, col3 = st.columns([1, 2, 1])  # Adjust column widths for better centering
-with col2:
-    st.image(logo, width=120)  # Adjust width as needed
+# col1, col2, col3 = st.columns(3)  # Adjust column widths for better centering
+# with col2:
+#     st.image(logo, width=120)  # Adjust width as needed
+
+col1, col2, col3 = st.columns(3)
+col2.image(logo, width=120)
 
 # st.image("Cervera Logo BWG.png", width=120)
 st.markdown('<div class="title">Portfolio Management</div>', unsafe_allow_html=True)
