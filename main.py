4b2559d3b12e0bdaace983d0289adbb8f041745d
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-12-13 12:26:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -43,12 +43,13 @@ st.markdown(custom_css, unsafe_allow_html=True)
 
 hide_github_icon = """
 <style>
-.viewerBadge_container__1QSob {  /* Adjust the class name as needed */
-    display: none;
-}
+    [data-testid="stViewerBadge"] {
+        display: none;
+    }
 </style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 
 # Function to generate Prophet forecast plot for a given stock ticker
